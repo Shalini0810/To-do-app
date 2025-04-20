b490b181b64976bdba266024d43fccabaeda4516
Shalini0810 <shalinikotha@Shalinis-MacBook-Air.local>
2025-04-20 22:32:15 +0530

    added models file

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,16 +2,23 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
+        unique: true,
         required : true,
     },
     username: {
         type: String,
-        required : true,
+        unique: true,
     },
     password: {
         type: String,
         required : true,
     },
+    list: [
+        {
+        type: mongoose.Types.ObjectId,
+        ref: "List",
+    },
+   ],
 });
 
 module.exports = mongoose.model("User", userSchema);
